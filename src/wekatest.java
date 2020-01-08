735c9de0c391ca225219139fd1fc944f76a2f12b
trevorsaudi <38811409+trevorsaudi@users.noreply.github.com>
2020-01-08 16:31:47 +0300

    the weka api to diagnose diseases

---
+++ src/wekatest.java
@@ -0,0 +1,111 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.NominalPrediction;
+import weka.classifiers.functions.Logistic;
+import weka.classifiers.rules.DecisionTable;
+import weka.classifiers.rules.OneR;
+import weka.classifiers.rules.PART;
+import weka.classifiers.trees.DecisionStump;
+import weka.classifiers.trees.J48;
+import weka.core.FastVector;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
+public class wekatest {
+    public static BufferedReader readDataFile(String filename) {
+        BufferedReader inputReader = null;
+        
+        try {
+            inputReader = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found: " + filename);
+        }
+        
+        return inputReader;
+    }
+    
+    public static Evaluation simpleClassify(Classifier model, Instances trainingSet, Instances testingSet) throws Exception {
+        Evaluation validation = new Evaluation(trainingSet);
+        
+        model.buildClassifier(trainingSet);
+        validation.evaluateModel(model, testingSet);
+        
+        return validation;
+    }
+    
+    public static double calculateAccuracy(FastVector predictions) {
+        double correct = 0;
+        
+        for (int i = 0; i < predictions.size(); i++) {
+            NominalPrediction np = (NominalPrediction) predictions.elementAt(i);
+            if (np.predicted() == np.actual()) {
+                correct++;
+            }
+        }
+        
+        return 100 * correct / predictions.size();
+    }
+    
+    public static Instances[][] crossValidationSplit(Instances data, int numberOfFolds) {
+        Instances[][] split = new Instances[2][numberOfFolds];
+        
+        for (int i = 0; i < numberOfFolds; i++) {
+            split[0][i] = data.trainCV(numberOfFolds, i);
+            split[1][i] = data.testCV(numberOfFolds, i);
+        }
+        
+        return split;
+    }
+    
+    public static void main(String[] args) throws Exception {
+    
+        BufferedReader datafile = readDataFile("C:\\Program Files (x86)\\Weka-3-8\\data/diabetes.arff");
+        
+        
+        
+        
+        Instances data = new Instances(datafile);
+        data.setClassIndex(data.numAttributes() - 1);
+        
+        // Choose a type of validation split
+        Instances[][] split = crossValidationSplit(data, 10);
+        
+        // Separate split into training and testing arrays
+        Instances[] trainingSplits = split[0];
+        Instances[] testingSplits  = split[1];
+        
+        // Choose a set of classifiers
+        Classifier[] models = {     new J48(),
+                                    new Logistic(),
+                                    new NaiveBayes()
+        };
+        // Run for each classifier model
+        for(int j = 0; j < models.length; j++) {
+
+            // Collect every group of predictions for current model in a FastVector
+            FastVector predictions = new FastVector();
+            
+            // For each training-testing split pair, train and test the classifier
+            for(int i = 0; i < trainingSplits.length; i++) {
+                Evaluation validation = simpleClassify(models[j], trainingSplits[i], testingSplits[i]);
+                predictions.appendElements(validation.predictions());
+                
+                // Uncomment to see the summary for each training-testing pair.
+                // System.out.println(models[j].toString());
+            }
+            
+            // Calculate overall accuracy of current classifier on all splits
+            double accuracy = calculateAccuracy(predictions);
+            
+            // Print current classifier's name and accuracy in a complicated, but nice-looking way.
+            System.out.println(models[j].getClass().getSimpleName() + ": " + String.format("%.2f%%", accuracy) + "\n=====================");
+         
+        }
+        
+    }
+}
